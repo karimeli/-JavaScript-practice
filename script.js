963acdf0349118add2ce3ad4a097310718f2596f
karimeli <141066459+karimeli@users.noreply.github.com>
2025-06-04 09:53:27 -0600

    imagen y otros

---
+++ script.js
@@ -0,0 +1,25 @@
+
+const leftEye = document.getElementById('left-eye');
+const rightEye = document.getElementById('right-eye');
+const emojiContainer = document.getElementById('emoji-container');
+
+
+function moveEyes(event) {
+  const emojiContainerRect = emojiContainer.getBoundingClientRect();
+  
+ 
+  const mouseX = event.clientX - emojiContainerRect.left;
+  const mouseY = event.clientY - emojiContainerRect.top;
+
+
+  const eyeCenterX = emojiContainerRect.width / 2;
+  const eyeCenterY = emojiContainerRect.height / 2;
+
+  const angleLeft = Math.atan2(mouseY - eyeCenterY, mouseX - (eyeCenterX - 25));
+  const angleRight = Math.atan2(mouseY - eyeCenterY, mouseX - (eyeCenterX + 25));
+
+  leftEye.style.transform = `rotate(${angleLeft}rad)`;
+  rightEye.style.transform = `rotate(${angleRight}rad)`;
+}
+
+document.addEventListener('mousemove', moveEyes);
